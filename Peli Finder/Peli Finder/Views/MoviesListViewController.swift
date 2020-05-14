07c6e345a6f96c66a2ae7c975fdfb3a6fcf3943f
Jose Enrique Montañez Villanueva <jose_kikin_5@hotmail.com>
2020-05-14 14:43:22 -0500

    Agregación de los primeros archivos base

---
+++ Peli Finder/Peli Finder/Views/MoviesListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Peli Finder
+//
+//  Created by Jose Enrique Montañez Villanueva on 14/05/20.
+//  Copyright © 2020 Jose Enrique Montañez Villanueva. All rights reserved.
+//
+
+import UIKit
+
+class MoviesListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupViews()
+    }
+
+    
+    private func setupViews() {
+        
+    }
+
+}
+
+// MARK: - Data source
+extension MoviesListViewController {
+    
+}
+
+
+// MARK: - Delegate
+
+extension MoviesListViewController {
+    
+}
+
